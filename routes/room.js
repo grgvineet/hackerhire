b6bad2c0d2184a03722d891fdc027efd48ec4813
sidharth <sidharth15.raja@gmail.com>
2016-04-07 13:13:19 +0530

    Increment

--- routes/room.js
+++ routes/room.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
+/* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', "*");
-  res.render('index', { title: 'Express' });
+  res.header('Access-Control-Allow-Origin',"*");
+  res.render('room',{title:'Express'});
 });
 
 module.exports = router;
