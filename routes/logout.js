0a3c39adff2fbf0ffedfefc52f740adab0093bb6
Vineet Garg <grgvineet@gmail.com>
2016-04-13 00:58:41 +0530

    Destroy sessions on logout Not destroying sessions manually causes huge number of sessions in database

--- routes/logout.js
+++ routes/logout.js
@@ -3,6 +3,7 @@ var router = express.Router();
 
 router.get('/', function(req, res, next) {
     if (req.isAuthenticated()) {
+        req.session.destroy(null);
         req.logOut();
     }
     res.redirect('/');
